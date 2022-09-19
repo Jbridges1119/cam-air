59aa13c38a807c51fdcc8291b260912882c515cc
Jeff <jbridges1119@gmail.com>
2022-09-19 13:42:46 -0600

    Add NavBar and landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-
-import './App.css';
+import { ThemeProvider } from "@mui/material";
+import Footer from "./Footer";
+import NavBar from "./NavBar";
+import theme from "./Styles/theme";
+import LandingPage from './Pages/01_LandingPage/LandingPage'
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <ThemeProvider theme={theme}>
+    <NavBar/>
+    <LandingPage/>
+    {/* <Footer/> */}
+    </ThemeProvider>
   );
 }
 
