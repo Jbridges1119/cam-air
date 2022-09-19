5fdca690893494303d55a6516ba2453fbf718ba6
Jeff <jbridges1119@gmail.com>
2022-09-19 17:03:04 -0600

    Add footer components

--- src/Pages/07_Footer/Footer.tsx
+++ src/Pages/07_Footer/Footer.tsx
@@ -1,12 +1,54 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, CardMedia, Divider, Grid, Typography } from "@mui/material";
 import React from "react";
-import Banner from './components/Banner'
+import Banner from "./components/Banner";
+import SocialsFooter from './components/SocialsFooter'
+import ContactUsFooter from './components/ContactUsFooter'
+import CompanyFooter from './components/CompanyFooter'
 const Footer = () => {
   return (
-    <>
-     <Banner/>
-      <Box height={"50vh"}></Box>
-    </>
+    <Box sx={{display:'flex', flexDirection:'column', width:'100%'}}>
+      <Banner />
+      <Box
+        sx={{
+          
+         
+          display: "flex",
+          justifyContent: "center",
+          alignItems: 'center',
+
+        }}
+      >
+        <Grid
+          container
+          direction="row"
+          justifyContent={{sm:"space-between", xs:'center'}}
+          alignItems="flex-start"
+          sx={{maxWidth: "1200px",}}
+        >
+          <Grid item lg={3} md={3} sm={6} xs={10}>
+          <ContactUsFooter/>
+          </Grid>
+          <Grid item lg={3} md={3} sm={6} xs={10} >
+          <CompanyFooter/>
+          </Grid>
+          <Grid item lg={3} md={3} sm={6} xs={10}>
+          <SocialsFooter/>
+          </Grid>
+      
+        </Grid>
+      </Box>
+      <Box>  </Box>
+      <Divider />
+      <Button
+        sx={{ all: "unset", cursor: "pointer" }}
+        href="https://github.com/Jbridges1119/cam-air"
+        target="_blank"
+      >
+        <Typography variant="body2" textAlign={'center'} color={"black"}>
+          Designed and Built By Jeff Bridges
+        </Typography>
+      </Button>
+    </Box>
   );
 };
 
