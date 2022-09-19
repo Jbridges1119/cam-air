e003962e2987f41c6bc2f8d37736c73807a31262
Jeff <jbridges1119@gmail.com>
2022-09-19 13:44:33 -0600

    Create Header

---
+++ src/Pages/01_LandingPage/LandingPage.tsx
@@ -0,0 +1,63 @@
+import { Box, Button, CardMedia, Grid, Stack, Typography } from "@mui/material";
+import React from "react";
+import banner from "../../Assests/headers/main.jpg";
+const LandingPage = () => {
+  return (
+    <>
+      <Box
+        sx={{
+          backgroundImage: `url(${banner})`,
+
+          minHeight: "50vh",
+          width: "100%",
+          boxSizing: "border-box",
+          backgroundSize: "cover",
+          display: "flex",
+          flexDirection: "column",
+          overflowX: "hidden",
+          pt: { sm: "10%", xs: "30%" },
+          pb: "18%",
+          mt: { sm: 0, xs: "45px" },
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Box sx={{ maxWidth: "1400px" }}>
+          <Grid
+            height={"75%"}
+            container
+            direction="row"
+            justifyContent={{sm:"flex-start",xs:'center'}}
+            alignItems="center"
+          >
+            <Grid item xs={0} sm={1} md={0}></Grid>
+            <Grid item xs={10} sm={8} md={4.8} ml={2} mb={22}>
+              <Stack spacing={2}>
+                <Typography
+                  variant="h2"
+                  sx={{ color: "white", fontWeight: "bold" }}
+                >
+                  Cam Air Refigeration and Heating provides commercial HVAC and
+                  refrigeration.
+                </Typography>
+                <Typography variant="h6" sx={{ color: "white" }}>
+                  With over 50 years of combined experience, our team delivers
+                  quality, timely services that leads to greater energy
+                  efficiency and reduces operational costs for our customers.
+                </Typography>
+                <Button variant="contained"
+                sx={{p:'11px 20px', fontSize:{sm:'25px', xs:'18px'},maxWidth:'400px'}}
+                >Schedule An Appointment</Button>
+              
+              </Stack>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+
+      <Box sx={{ height: "2000px" }} />
+    </>
+  );
+};
+
+export default LandingPage;
