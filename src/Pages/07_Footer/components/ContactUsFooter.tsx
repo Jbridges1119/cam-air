0295a101feacf9ff4fd4c15667b0b6f9558a3790
Jeff <jbridges1119@gmail.com>
2022-09-19 17:02:28 -0600

    Add company info

---
+++ src/Pages/07_Footer/components/ContactUsFooter.tsx
@@ -0,0 +1,46 @@
+import { Box, Divider, Typography } from "@mui/material";
+import React from "react";
+
+const ContactUsFooter = () => {
+  return (
+    <Box p={3}>
+      <Typography variant="h6" sx={{ color: "#002685" }}>
+        Contact Us
+      </Typography>
+      <Divider />
+      <Typography variant="body1" fontWeight="bold">
+        Address
+      </Typography>
+      <Typography variant="subtitle1" sx={{ lineHeight: 1.2 }}>
+        313 13 St N, <br />
+        Lethbridge, AB
+        <br />
+        T1H 2R9
+      </Typography>
+      <Typography variant="body1" fontWeight="bold">
+        Phone
+      </Typography>
+      <Typography variant="subtitle1" sx={{ lineHeight: 1.2 }}>
+        Main 403-320-6690
+        <br />
+        Fax 403-327-1432
+      </Typography>
+      <Typography variant="body1" fontWeight="bold">
+        Email
+      </Typography>
+      <Typography variant="subtitle1" sx={{ lineHeight: 1.2 }}>
+        info@camairrefrigeration.com
+      </Typography>
+      <Typography variant="body1" fontWeight="bold">
+        Business Hours
+      </Typography>
+      <Typography variant="subtitle1" sx={{ lineHeight: 1.2 }}>
+        Monday – Friday: 8:00 AM - 4:30 PM
+        <br />
+        24/7 Emergency Service
+      </Typography>
+    </Box>
+  );
+};
+
+export default ContactUsFooter;
