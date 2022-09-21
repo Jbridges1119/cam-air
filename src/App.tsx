c8f37d2e61d163c42476eca3e928c2e5b15d6953
Jeff <jbridges1119@gmail.com>
2022-09-21 12:11:41 -0600

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import { ThemeProvider } from "@mui/material";
 import Footer from "./Pages/07_Footer/Footer";
 import NavBar from "./NavBar";
 import theme from "./Styles/theme";
-import LandingPage from './Pages/01_LandingPage/LandingPage'
+import LandingPage from "./Pages/01_LandingPage/LandingPage";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-    <NavBar/>
-    <LandingPage/>
-    <Footer/>
+      <NavBar />
+      <Routes>
+      <Route path="/" element={<LandingPage />} />
+      </Routes>
+      <Footer />
     </ThemeProvider>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,16 @@ import './Styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Provider from './Context/sharedContext'
-
+import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
      <Provider>
+     <BrowserRouter>
     <App />
+    </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
