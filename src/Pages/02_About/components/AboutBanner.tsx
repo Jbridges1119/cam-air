5323687361d4b09992f90929da663780435a3904
Jeff <jbridges1119@gmail.com>
2022-09-21 17:01:49 -0600

    Add culture banner

---
+++ src/Pages/02_About/components/AboutBanner.tsx
@@ -0,0 +1,89 @@
+//MUI
+import { Box, Grid, Stack, Typography } from "@mui/material";
+//Style
+import warmDay from "../../../Assets/stock/warmDay.jpg";
+
+const bannerHeightSX = { height: "600px" };
+const AboutBanner = () => {
+  return (
+    <Box
+      sx={{
+        width:"100%",
+        background: "#002685",
+        ...bannerHeightSX,
+      }}
+    >
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid item xs={0} md={6}>
+          <Box
+            sx={{
+              backgroundImage: `url(${warmDay})`,
+              ...bannerHeightSX,
+              width: "100%",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              display: "flex",
+              justifyContent: "flex-end",
+            }}
+          >
+            <Box
+              sx={{
+                clipPath: `polygon(0 0, 100% 0%, 100% 100%, 100% 100%)`,
+                ...bannerHeightSX,
+                width: "20%",
+                background: "#002685",
+              }}
+            ></Box>
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Box
+            sx={{
+              ...bannerHeightSX,
+              width: "100%",
+            }}
+          >
+            <Stack
+              spacing={3}
+              sx={{
+                my: { sm: 4, xs: 0 },
+                py: { xs: 4, lg: 10 },
+                maxWidth: "550px",
+                mx: 4,
+              }}
+            >
+              <Typography
+                variant="h6"
+                sx={{ color: "white", fontWeight: "lighter" }}
+              >
+                Helping other small businesses achieve their goals
+              </Typography>
+              <Typography
+                variant="h5"
+                sx={{ color: "white", fontWeight: "bold" }}
+              >
+                Work is not just something we do, it’s what we love.
+              </Typography>
+              <Typography
+                variant="h6"
+                sx={{ color: "white", fontWeight: "lighter" }}
+              >
+              Our team works collaboratively, sharing information to bring a “can do” approach to any opportunity or problem. Together, we aim to achieve the best results and customer service each and every time.
+              <br/><br/>
+              We have a passion for the work we do and our employees exemplify this every day. This passion coupled with an uncompromising commitment to excellence drives everything we do.
+              </Typography>
+           
+            </Stack>
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
+
+export default AboutBanner
