b09e29f38df9b7db4c76d5d9b7f65539e0034a3a
Jeff <jbridges1119@gmail.com>
2022-09-21 17:01:17 -0600

    Add info to about us section

---
+++ src/Pages/02_About/components/AboutIntro.tsx
@@ -0,0 +1,38 @@
+import { Box, Typography } from '@mui/material'
+import React from 'react'
+
+const AboutIntro = () => {
+  const flexCenterSX = {
+    display:'flex',
+    justifyContent:'center',
+    alignContent:'center',
+    alignItems:'center'
+  }
+  return (
+    <Box sx={{...flexCenterSX, mt:6}}>
+      <Box sx={{maxWidth:'1200px',
+          width: {xs:"90%",lg:'100%'},
+          display: "flex",
+          flexDirection: "column",}}>
+      <Typography variant='h6' color={"#002685"}>A Local business. Serving local businesses</Typography>
+      
+      <Typography variant='h3'   fontWeight={'bold'} py={2}>At Cam Air, we’re focused on providing the best possible customer service in safe, effective, and innovative ways.</Typography>
+      <Typography variant='h6' fontWeight={'lighter'}  pt={2} mb={10}> Founded in 1992 by Mark Cameron, Cam Air Refrigeration & Heating is a privately owned family run company that has established itself as a leader in the commercial refrigeration, heating and air conditioning industry (HVACR). <br/><br/>
+      It was Marks goal to provide the best personal level of service uncommon to their customers and through-out the decades this has become the foundation for Cam Air's success and continues to guide us to this day.
+      <br/><br/>
+      Working as One Team with customers and each other, Cam Air Refrigeration and Heating leverages our expertise and work ethic to provide responsive mechanical contracting solutions to commercial, and agricultural customers across southern Alberta.
+      <br/><br/>
+      Our experienced group of team members ensure that:
+      
+      <ul>
+        <li>Our mechanical systems run efficiently, leaving our customers with peace of mind and time to focus on being productive</li>
+        <li>Our employees are highly trained in their area of expertise and provide quality service to our customers</li>
+        <li>Our company embraces innovation, remains financially strong, and honours our commitments to customers and community</li>
+      </ul>
+      </Typography>
+
+    </Box></Box>
+  )
+}
+
+export default AboutIntro
---
+++ src/Pages/02_About/components/AboutSafety.tsx
@@ -0,0 +1,92 @@
+import { Box, CardMedia, Grid, Typography } from "@mui/material";
+import React from "react";
+import family from "../../../Assets/stock/family.jpg";
+const AboutSafety = () => {
+  const flexCenterSX = {
+    display: "flex",
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
+  };
+  return (
+    <Box sx={{ ...flexCenterSX, mt: 6 }}>
+      <Box
+        sx={{
+          maxWidth: "1200px",
+          width: {xs:"90%",lg:'100%'},
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <Typography variant="h3" fontWeight={"bold"} py={2}>
+          We believe everyone has the right to go home when the job is done and
+          one accident is one too many.
+        </Typography>
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Grid item sm={7} xs={11}>
+            <Typography variant="h6" fontWeight={"lighter"} mb={4}>
+              {" "}
+              Health and safety is an integral part of our corporate culture,
+              from the owner to the staff everyone is responsible for ensuring
+              the health and safety of our employees, subcontractors, clients
+              and the public.
+              <br />
+              <br />
+              By providing resources and training, encouraging teamwork,
+              reinforcing positive behaviors, incorporating applicable
+              legislation and adopting industry best practices, Gateway
+              continuously monitors, audits and improves our health and safety
+              program.
+            </Typography>
+          </Grid>
+          <Grid item sm={5} xs={12}>
+            <CardMedia
+              sx={{
+                backgroundImage: `url(${family})`,
+                height: "300px",
+                width: "100%",
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                display: "flex",
+                justifyContent: "flex-end",
+              }}
+            />
+          </Grid>{" "}
+        </Grid>
+
+        <Typography variant="h4" fontWeight={"bold"} py={2}>
+          Cam Air is committed to:
+        </Typography>
+        <Typography variant="h6" fontWeight={"lighter"} mb={10}>
+          <ul>
+            <li>
+              Work together with our employees to consult, cooperate and achieve
+              excellence in safety performance.
+            </li>
+            <li>
+              Provide proper training, equipment, systems, and tools to ensure
+              the health and safety of our employees and subcontractors.
+            </li>
+            <li>
+              Continuously assess, audit and improve our health and safety
+              program to reduce the risk of injury in a changing work
+              environment.
+            </li>
+            <li>
+              Protect the environment from any adverse effects of our operations
+              and recycle, as well as promote the use of recycled products where
+              possible.
+            </li>
+          </ul>
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export default AboutSafety;
