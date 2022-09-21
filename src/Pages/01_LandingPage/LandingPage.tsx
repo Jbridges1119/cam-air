215ad41d11788a31d7f8f3414e371ccce3145637
Jeff <jbridges1119@gmail.com>
2022-09-21 03:29:27 -0600

    Add intro statement

--- src/Pages/01_LandingPage/LandingPage.tsx
+++ src/Pages/01_LandingPage/LandingPage.tsx
@@ -1,12 +1,16 @@
 import { Box,  } from "@mui/material";
 import Header from './components/Header'
 import ReviewCarousel from './components/Reviews'
+import Introduction from './components/Introduction'
+import ServiceBanner from './components/ServiceBanner'
 const LandingPage = () => {
   return (
     <>
       <Header/>
+    <Introduction/>
 
       <Box sx={{ height: "1000px" }} />
+      <ServiceBanner/>
       <ReviewCarousel/>
     </>
   );
