e6b91123c8aeea384d8b6d55f2fd491fbc9a6bf4
Jeff <jbridges1119@gmail.com>
2022-09-21 15:07:24 -0600

    Add about page

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import NavBar from "./NavBar";
 import theme from "./Styles/theme";
 import LandingPage from "./Pages/01_LandingPage/LandingPage";
 import { Route, Routes } from "react-router-dom";
-
+import About from './Pages/02_About/About'
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <NavBar />
       <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/about" element={<About/>} />
       </Routes>
       <Footer />
     </ThemeProvider>
