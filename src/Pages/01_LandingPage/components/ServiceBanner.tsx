771df5675f4f884ba09de5fa2f3f2bda347f59bc
Jeff <jbridges1119@gmail.com>
2022-09-21 12:13:09 -0600

    set banner height to variable

--- src/Pages/01_LandingPage/components/ServiceBanner.tsx
+++ src/Pages/01_LandingPage/components/ServiceBanner.tsx
@@ -1,12 +1,15 @@
-import { Box, Button, Grid, Stack, Typography } from "@mui/material";
-import React from "react";
-import tools from "../../../Assests/stock/tools.jpg";
+//MUI
+import { Box, Grid, Link, Stack, Typography } from "@mui/material";
+//Style
+import tools from "../../../Assets/stock/tools.jpg";
+
+const bannerHeightSX = { height: "600px" };
 const ServiceBanner = () => {
   return (
     <Box
       sx={{
         background: "#002685",
-        height: "500px",
+        ...bannerHeightSX,
       }}
     >
       <Grid
@@ -19,7 +22,7 @@ const ServiceBanner = () => {
           <Box
             sx={{
               backgroundImage: `url(${tools})`,
-              height: "500px",
+              ...bannerHeightSX,
               width: "100%",
               backgroundSize: "cover",
               backgroundPosition: "center",
@@ -30,7 +33,7 @@ const ServiceBanner = () => {
             <Box
               sx={{
                 clipPath: `polygon(0 0, 100% 0%, 100% 100%, 100% 100%)`,
-                height: "500px",
+                ...bannerHeightSX,
                 width: "20%",
                 background: "#002685",
               }}
@@ -40,15 +43,46 @@ const ServiceBanner = () => {
         <Grid item xs={12} md={6}>
           <Box
             sx={{
-              height: "500px",
+              ...bannerHeightSX,
               width: "100%",
             }}
           >
-            <Stack spacing={2} p={4} maxWidth={'600px'}>
-              <Typography variant="h6"sx={{color:"white", fontWeight:'lighter'}}>Your Preventative Maintenance Partner</Typography>
-              <Typography variant="h6"sx={{color:"white", fontWeight:'bold'}}>Forget downtime. We’re available around the clock, 365 days a year, to keep your business running smoothly.</Typography>
-              <Typography variant="h6"sx={{color:"white",fontWeight:'lighter' }}>Whether it’s an emergency repair or general service and maintenance Gateway’s highly trained team of certified HVAC, refrigeration, plumbing and electrical tradespeople are ready to get the job done right.</Typography>
-              <Typography variant="h6"sx={{color:"white",fontWeight:'lighter' }}><Button>Learn more about our maintenance services</Button></Typography>
+            <Stack
+              spacing={4}
+              sx={{
+                my: { sm: 4, xs: 0 },
+                py: { xs: 4, sm: 10 },
+                maxWidth: "550px",
+                mx: 4,
+              }}
+            >
+              <Typography
+                variant="h6"
+                sx={{ color: "white", fontWeight: "lighter" }}
+              >
+                WE ARE YOUR PREVENTATIVE MAINTENANCE PARTNER
+              </Typography>
+              <Typography
+                variant="h5"
+                sx={{ color: "white", fontWeight: "bold" }}
+              >
+                Forget downtime. We’re available around the clock, 365 days a
+                year, to keep your business running smoothly.
+              </Typography>
+              <Typography
+                variant="h6"
+                sx={{ color: "white", fontWeight: "lighter" }}
+              >
+                Whether it’s an emergency repair, or general service and
+                maintenance, Cam Air’s highly trained team of Certified HVAC and
+                Refrigeration Tradespeople are ready to get you back up and
+                running as fast as possible without compromising on quality.
+              </Typography>
+              <Link href="#" underline="always">
+                <Typography variant="body1" sx={{ color: "white" }}>
+                  Learn more about our maintenance services
+                </Typography>
+              </Link>
             </Stack>
           </Box>
         </Grid>
