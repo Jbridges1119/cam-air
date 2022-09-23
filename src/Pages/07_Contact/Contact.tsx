f655643035fb649e6d206af6feb8c04238392d05
Jeff <jbridges1119@gmail.com>
2022-09-23 14:56:58 -0600

    Add contact form

--- src/Pages/07_Contact/Contact.tsx
+++ src/Pages/07_Contact/Contact.tsx
@@ -4,11 +4,13 @@ import { Box } from '@mui/material'
 //Components
 import ContactHeader from './components/ContactHeader'
 import Location from './components/Location'
+import ContactForm from './components/ContactForm'
 const Contact = () => {
   return (
     <Box sx={{width:'100%', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
       <ContactHeader/>
       <Location/>
+      <ContactForm/>
       <Box height={'100vh'}/>
     </Box>
   )
