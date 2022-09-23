f32874685cf17979c3b5d7058bcf7b08080440ac
Jeff <jbridges1119@gmail.com>
2022-09-23 17:43:07 -0600

    Add scroll to top component

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,21 @@ import LandingPage from "./Pages/01_LandingPage/LandingPage";
 import { Route, Routes } from "react-router-dom";
 import About from './Pages/02_About/About'
 import Contact from './Pages/07_Contact/Contact'
+import ScrollToTop from "./Hooks/ScrollToTop";
+
+
 function App() {
+
   return (
     <ThemeProvider theme={theme}>
       <NavBar />
+      <ScrollToTop>
       <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/about" element={<About/>} />
       <Route path="/contact" element={<Contact/>} />
       </Routes>
+      </ScrollToTop>
       <Footer />
     </ThemeProvider>
   );
---
+++ src/Hooks/ScrollToTop.tsx
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router";
+
+interface Props {
+  children: React.ReactNode;
+}
+const ScrollToTop: React.FC<Props> = ({children}) => {
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  return <>{children}</>
+};
+
+export default ScrollToTop;
