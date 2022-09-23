f29c9e6e6767adfbcc8ac98f08b11e7a77b14b33
Jeff <jbridges1119@gmail.com>
2022-09-23 16:29:35 -0600

    Add map

--- src/Pages/07_Contact/Contact.tsx
+++ src/Pages/07_Contact/Contact.tsx
@@ -5,13 +5,15 @@ import { Box } from '@mui/material'
 import ContactHeader from './components/ContactHeader'
 import Location from './components/Location'
 import ContactForm from './components/ContactForm'
+import GoogleMap from './components/GoogleMap'
 const Contact = () => {
   return (
     <Box sx={{width:'100%', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
       <ContactHeader/>
       <Location/>
       <ContactForm/>
-      <Box height={'100vh'}/>
+      <GoogleMap/>
+     
     </Box>
   )
 }
---
+++ src/Pages/07_Contact/components/GoogleMap.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GoogleMap = () => {
+  return (
+    <iframe title="google location" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1232868.5778481208!2d-113.55726644145338!3d50.2521378618072!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x536e87ba0c37a4f5%3A0x6acfb0d294a296ad!2s313%2013%20St%20N%2C%20Lethbridge%2C%20AB%20T1H%202R9!5e0!3m2!1sen!2sca!4v1663971620275!5m2!1sen!2sca" width='100%' height="600px" style={{border:0}} allowFullScreen={true} loading="lazy"></iframe>
+  )
+}
+
+export default GoogleMap
