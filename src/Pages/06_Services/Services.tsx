7bd8e0a838c1b2d81a0d2704a16b404cac022eea
Jeff <jbridges1119@gmail.com>
2022-09-26 11:31:51 -0600

    Add FAQ section

--- src/Pages/06_Services/Services.tsx
+++ src/Pages/06_Services/Services.tsx
@@ -6,7 +6,7 @@ import ServicesHeader from './components/ServicesHeader';
 import ServicesIntro from './components/ServicesIntro';
 import ServicesBanner from './components/ServicesBanner';
 import ServiceSupport from './components/ServiceSupport';
-
+import FAQ from './components/FAQ';
 const BoxSX = {width:'100%', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'};
 
 const Services = () => {
@@ -16,6 +16,7 @@ const Services = () => {
     <ServicesIntro/>
     <ServicesBanner/>
     <ServiceSupport/>
+    <FAQ/>
 </Box>
   )
 }
