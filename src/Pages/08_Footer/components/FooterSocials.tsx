b34b36d0269f841f005b1de7e0b6e60f05377d4d
Jeff <jbridges1119@gmail.com>
2022-09-26 12:41:38 -0600

    Add instagram button

--- src/Pages/08_Footer/components/FooterSocials.tsx
+++ src/Pages/08_Footer/components/FooterSocials.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 //MUI
-import { Box, Button, CardMedia, Divider, Grid } from "@mui/material";
+import { Box, Button, CardMedia, Divider, Grid, styled } from "@mui/material";
 //Style
 import logo from "../../../Assets/logo2.png";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
 import theme from "../../../Styles/theme";
 
 const iconSX = {
@@ -17,8 +18,9 @@ const iconSX = {
     color: theme.palette.primary.main,
   },
 };
-
-
+const ImgButton = styled(Button)`
+max-height: 60px;
+`
 const GridSX = { display: "flex", justifyContent: "center" };
 const SocialsFooter = () => {
   return (
@@ -31,28 +33,25 @@ const SocialsFooter = () => {
       />
       <Divider />
       <Grid container direction="row">
-        <Grid item xs={4} sx={GridSX}>
-          <Button sx={{ height: "60px" }}>
-            <FacebookIcon sx={iconSX}></FacebookIcon>
-          </Button>
+        <Grid item xs={3} sx={GridSX}>
+          <ImgButton >
+            <FacebookIcon sx={iconSX}/>
+          </ImgButton>
         </Grid>
-        <Grid item xs={4} sx={GridSX}>
-          <Button
-            sx={{ height: "60px" }}
-            href="https://twitter.com/intent/tweet?original_referer=https%3A%2F%2Fwww.camairrefrigeration.com%2F&ref_src=twsrc%5Etfw%7Ctwcamp%5Ebuttonembed%7Ctwterm%5Eshare%7Ctwgr%5E&text=Cam%20Air%20Refrigeration%20%26%20Heating%20%7C%2024%2F7%20Service%20Lethbridge%20%7C%20Home&url=https%3A%2F%2Fwww.camairrefrigeration.com%2F"
-            target="_blank"
-          >
+        <Grid item xs={3} sx={GridSX}>
+          <ImgButton >
             <TwitterIcon sx={iconSX} />
-          </Button>
+          </ImgButton>
         </Grid>
-        <Grid item xs={4} sx={GridSX}>
-          <Button
-            sx={{ height: "60px" }}
-            href="https://www.linkedin.com/in/Jbridges1119/"
-            target="_blank"
-          >
+        <Grid item xs={3} sx={GridSX}>
+          <ImgButton >
             <LinkedInIcon sx={iconSX} />
-          </Button>
+          </ImgButton>
+        </Grid>
+        <Grid item xs={3} sx={GridSX}>
+          <ImgButton >
+            <InstagramIcon sx={iconSX} />
+          </ImgButton>
         </Grid>
       </Grid>
     </Box>
